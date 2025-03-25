2eeb3674010a900756cae07b231047fba055d04e
leraTo20moloI <leratonthabiseng.moloi@gmail.com>
2025-03-25 13:47:49 +0200

    Add files via upload

---
+++ python week2.py
@@ -0,0 +1,23 @@
+my_list = []
+print(my_list)
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list = [10,20,30,40]
+my_list.insert(1,15)
+print(my_list)
+my_list = [10, 15, 20, 30, 40]
+my_list.extend([50, 60, 70])
+print(my_list)
+my_list = [10, 15, 20, 30, 40, 50, 60, 70]
+my_list.pop()
+print(my_list)
+my_list = [10, 15, 20, 30, 40, 50, 60]
+my_list.sort()
+print(my_list)
+my_list = [10, 15, 20, 30, 40, 50, 60]
+index_of_30 = my_list.index(30)
+print("The index of 30 is:", index_of_30)
